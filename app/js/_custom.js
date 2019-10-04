8fdf46db824d706e26a59995f4f645cd1df56b7b
Anton <omega24v1993@gmail.com>
2019-10-04 18:05:01 +0300

    add project

---
+++ app/js/_custom.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	// MENU & NAVBAR
+	// (function (){
+	// 	var headerResize = $('header').data('header-resize');
+
+	// 	if( headerResize == 1 ) {
+	// 		$(window).bind('scroll', toggleNavClass);
+	// 	}
+		
+	// 	function toggleNavClass() {
+	// 		var scrollTop = $(window).scrollTop();
+	// 		var windowWidth = $(window).width();
+	// 		$('header').toggleClass( 'fixed-top', scrollTop > 100 );
+	// 	}
+
+	// 	$(".navbar-nav a").click(function(e) {
+	// 		e.preventDefault();
+	// 		var t = $(this).attr("href");
+	// 		$("body, html").stop(!0, !0).animate({
+	// 			scrollTop: $(t).offset().top - 120
+	// 		}, 1e3)
+	// 	});
+
+	// 	$(document).click(function(e) {
+	// 		if (!$(e.target).is('nav')) {
+	// 			$('.collapse').collapse('hide');	    
+	// 		}
+	// 	});
+	// }())
+
+});
